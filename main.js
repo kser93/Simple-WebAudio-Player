19cecbc168ee3d55580acf0035db14c067479ba9
Сергей Карбовский <kserx05@rambler.ru>
2015-07-09 10:55:31 +0300

    Example with load and play file

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-/**
- * Created by Сергей on 09.07.2015.
- */
+var audioCtx, buf;
+
+var initializeAudioContext = function() {
+    audioCtx = new window.AudioContext();
+    loadExample();
+};
+
+function loadExample() {
+    var req = new XMLHttpRequest();
+    req.open("GET","italiano.mp3",true);
+    req.responseType = "arraybuffer";
+    req.onload = function() {
+        audioCtx.decodeAudioData(req.response, function(buffer) {
+            buf = buffer;
+            playExample();
+        });
+    };
+    req.send();
+}
+
+function playExample() {
+    var src = audioCtx.createBufferSource();
+    var gainNode = audioCtx.createGain();
+
+    src.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    src.buffer = buf;
+    src.start();
+}
+
+window.addEventListener('load', initializeAudioContext, false);
