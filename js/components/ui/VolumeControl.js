8543e9d35c9e8a9440c75b969b63953375dad953
Сергей Карбовский <kserx05@rambler.ru>
2015-07-16 06:15:41 +0300

    рефакторинг скрипта компонента VolumeControl в jquery-стиле

--- js/components/ui/VolumeControl.js
+++ js/components/ui/VolumeControl.js
@@ -5,9 +5,9 @@ define(
     function(
         Backbone
     ) {
-        var VolumeControl = _.extend({
-            $el: $('.player__controls__volume'),
+        var $el = $('.player__controls__volume');
 
+        var VolumeControl = _.extend({
             displayVolume: function(volume) {
                 if (volume < 0) {
                     volume = 0;
@@ -18,7 +18,7 @@ define(
                 var degrees = volume * 3.6;
 
                 if (degrees <= 180){
-                    this.$el.css(
+                    $el.css(
                         'background',
                         'linear-gradient(' + (270+degrees) + 'deg, transparent 50%, #C0C0C0 50%),' +
                         'linear-gradient(270deg, #C0C0C0 50%, transparent 50%),' +
@@ -26,32 +26,32 @@ define(
                     );
                 }
                 else {
-                    this.$el.css(
+                    $el.css(
                         'background',
                         'linear-gradient(' + (degrees - 270) + 'deg, transparent 50%, #ED9939 50%),' +
                         'linear-gradient(90deg, #ED9939 50%, transparent 50%),' +
                         '#C0C0C0'
                     );
                 }
-                this.$el.find('[class$=value]').text(volume+'%');
+                $el.find('[class$=value]').text(volume+'%');
             }
         }, Backbone.Events);
 
         var setVolume = function(e) {
-            var target= {
-                    x: e.pageX - VolumeControl.$el.offset().left,
-                    y: e.pageY - VolumeControl.$el.offset().top
+            var degree = 0,
+                target= {
+                    x: e.pageX - $el.offset().left,
+                    y: e.pageY - $el.offset().top
                 },
                 center = {
-                    x: VolumeControl.$el.width() / 2,
-                    y: VolumeControl.$el.height() / 2
+                    x: $el.width() / 2,
+                    y: $el.height() / 2
                 },
                 delta = {
                     x: target.x - center.x,
                     y: target.y - center.y
                 };
 
-            var degree = 0;
             if (delta.x <= 0 && delta.y > 0) {
                 //quarter 1
                 degree = Math.round(
@@ -90,18 +90,19 @@ define(
             return Math.round(degree / 3.6);
         };
 
-        VolumeControl.$el.mousedown(function() {
-            VolumeControl.$el.mousemove(function(e) {
-                VolumeControl.$el.css('cursor', 'default');
+        $el
+        .mousedown(function() {
+            $el
+            .mousemove(function(e) {
+                $el.css('cursor', 'default');
                 var volume = setVolume(e);
                 VolumeControl.displayVolume(volume);
+            })
+            .mouseup(function() {
+                $el.off('mousemove');
             });
-
-            VolumeControl.$el.mouseup(function() {
-                VolumeControl.$el.off('mousemove');
-            });
-        });
-        VolumeControl.$el.click(function(e) {
+        })
+        .click(function(e) {
             var volume = setVolume(e);
             VolumeControl.displayVolume(volume);
         });
